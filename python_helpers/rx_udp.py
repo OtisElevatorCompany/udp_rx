001fcc42d4b2e152c40ab81b2cc60c6b5b0511e2
Jeremy Mill <jeremy.mill@otis.com>
2018-08-24 11:36:45 -0400

    updating python helpers readme's/licensing

--- python_helpers/rx_udp.py
+++ python_helpers/rx_udp.py
@@ -1,3 +1,21 @@
+# Copyright 2018 Otis Elevator Company. All rights reserved.
+# Use of this source code is govered by the MIT license which
+# can be found in the LICENSE file.
+
+# Author: Jeremy Mill: jeremy.mill@otis.com
+
+# Otis udp_rx software has been designed to utilize information
+# security technology described Part 774 of the EAR Category 5 Part 2
+# but has been made publicly available in accordance with Part 742.15(b)
+# and is therefore not subject to U.S. export regulations.
+# Before you download this software be aware that the country in which you
+# are located may have restrictions related to the import, possession, use
+# and/or reexport of encryption items.  It is your responsibility to comply
+# with any applicable laws and regulations pertaining the import, possession,
+# use and/or reexport of encryption items.
+
+'''This script is for testing udp_rx recieve side functionality'''
+
 import socket
 
 UDP_IP = ""
