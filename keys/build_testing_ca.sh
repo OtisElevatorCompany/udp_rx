2a826b2921b1a8e300cd148b3f2ec0372d77980e
Jeremy Mill <jeremy.mill@otis.com>
2018-09-21 15:34:07 -0400

    changing cert.pem to just crt

--- keys/build_testing_ca.sh
+++ keys/build_testing_ca.sh
@@ -1,2 +1,2 @@
-openssl genrsa -out ca.key.pem 4096
-openssl req -key ca.key.pem -new -x509 -days 36500 -sha256 -extensions v3_ca -out ca.cert.pem
+openssl genrsa -out ca.key 4096
+openssl req -key ca.key -new -x509 -days 36500 -sha256 -extensions v3_ca -out ca.crt
