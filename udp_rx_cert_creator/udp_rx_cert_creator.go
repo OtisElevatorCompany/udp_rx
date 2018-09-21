fd11dd1052ebdb712ffc610b59ad337b18ad38aa
Jeremy Mill <jeremy.mill@otis.com>
2018-09-21 15:13:48 -0400

    updated default paths in cert creator and updated the gen keys readme

--- udp_rx_cert_creator/udp_rx_cert_creator.go
+++ udp_rx_cert_creator/udp_rx_cert_creator.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	// ca inputs
-	caKeyPathFlag := flag.String("keypath", "./keys/ca.key.pem", "path to the keyfile")
+	caKeyPathFlag := flag.String("keypath", "./ca.key.pem", "path to the keyfile")
 	caKeyPasswordFlag := flag.String("keypass", "", "password for private key if encrypted")
-	caCertPathFlag := flag.String("certpath", "./keys/ca.cert.pem", "path to the certfile")
+	caCertPathFlag := flag.String("certpath", "./ca.cert.pem", "path to the certfile")
 	// device key output
-	deviceKeyFlag := flag.String("devkey", "./udp_rx.key", "The output path for the udp_rx device key")
-	deviceCertFlag := flag.String("devcert", "./udp_rx.crt", "The oputput path for the udp_rx device cert")
+	deviceKeyFlag := flag.String("devkey", "/etc/udp_rx/udp_rx.key", "The output path for the udp_rx device key")
+	deviceCertFlag := flag.String("devcert", "/etc/udp_rx/udp_rx.crt", "The oputput path for the udp_rx device cert")
 	// parse args
 	flag.Parse()
 	// create the certs
