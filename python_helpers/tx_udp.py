222cc63fdc764644922bed497b5ca9e66d3a18b0
Jeremy Mill <jeremy.mill@otis.com>
2018-11-29 10:41:17 -0500

    updating tx to reflect header changes

--- python_helpers/tx_udp.py
+++ python_helpers/tx_udp.py
@@ -16,29 +16,30 @@
 
 '''This script is for testing udp_rx transmit side functionality
 including a "good" and a "bad" IP address'''
-
 import struct
 import socket
 
-IP = bytes([172,27,204,154])
+HEADER_VERSION = bytes([0x01, 0x00, 0x00])
+
+IP = bytes([192,168,56,101])
 IP_BAD = bytes([172,28,237,2])
-#gets the bottom 2 bytes of this 'struct' which is 50300 to 2 bytes
+
 PORT = struct.pack(">I", 50300)[2:]
 
-#junk data
-DATA = bytes([10,9,8,7,6,5,4,3,2,1,0])
+DATA = bytes([5,4,3,2,1])
+HEADER_GOOD = bytes([0x75]) + HEADER_VERSION + PORT + bytes([0x04]) + IP + bytes([0x80])
+HEADER_BAD = bytes([0x75]) + HEADER_VERSION + PORT + bytes([0x04]) + IP_BAD + bytes([0x80])
 
-#combine into one packet
-PACKET_CONTENTS = (IP + PORT + DATA)
-BAD_PACKET_CONTENTS = (IP_BAD + PORT + DATA)
 
+PACKET_GOOD = HEADER_GOOD + DATA
+PACKET_BAD = HEADER_BAD + DATA
 DEST = "127.0.0.1"
 DEST_PORT = 55555
 SOCK = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-for _ in range(0,10000):
-    SOCK.sendto(PACKET_CONTENTS, (DEST, DEST_PORT))
 for _ in range(0,100):
-    SOCK.sendto(BAD_PACKET_CONTENTS, (DEST, DEST_PORT))
+    SOCK.sendto(PACKET_GOOD, (DEST, DEST_PORT))
 for _ in range(0,100):
-    SOCK.sendto(PACKET_CONTENTS, (DEST, DEST_PORT))
+    SOCK.sendto(PACKET_BAD, (DEST, DEST_PORT))
+for _ in range(0,100):
+    SOCK.sendto(PACKET_GOOD, (DEST, DEST_PORT))
